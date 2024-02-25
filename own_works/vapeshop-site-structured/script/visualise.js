2f888f658a219952cea98f87852087a039a200af
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2024-02-25 13:13:46 +0200

    Add files via upload

---
+++ own_works/vapeshop-site-structured/script/visualise.js
@@ -0,0 +1,26 @@
+function visualiseProducts() {
+    var content = document.getElementById("hiddenContent1");
+    if (content.style.display === "none") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+
+function visualiseTeam() {
+    var content = document.getElementById("hiddenContent2");
+    if (content.style.display === "none") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+
+function visualiseActivity() {
+    var content = document.getElementById("hiddenContent3");
+    if (content.style.display === "none") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
